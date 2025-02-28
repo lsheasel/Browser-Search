4dd643175a13dbd40c61419dae06d4f4affc6ceb
Shease <83541534+lsheasel@users.noreply.github.com>
2025-02-28 16:04:49 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "CLI Browser Search wird installiert..."
+sudo cp search.py /usr/local/bin/search
+sudo chmod +x /usr/local/bin/search
+echo "Installation abgeschlossen! Starte mit 'search' im Terminal."
