74796dfdfe6c9822dad1c4dad8a9dbcf2451b1eb
Shease <83541534+lsheasel@users.noreply.github.com>
2025-02-28 16:14:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "CLI Browser Search wird installiert..."
-git clone https://github.com/shease-dev/cli-browser-search.git /tmp/cli-browser-search
-sudo cp /tmp/cli-browser-search/search.py /usr/local/bin/search
+sudo cp search.py /usr/local/bin/search
 sudo chmod +x /usr/local/bin/search
-rm -rf /tmp/cli-browser-search
-echo "Installation abgeschlossen ✅! Starte mit 'search' im Terminal."
+echo "Installation abgeschlossen! Starte mit 'search' im Terminal."
